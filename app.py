46ec98da2f4a49470fdbf4db54f57a87f41db996
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2022-02-07 02:19:39 -0500

    feat: added recommendations based on the content of items

--- app.py
+++ app.py
@@ -1,23 +1,70 @@
 from random import sample
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, redirect, request
 from cars.cars import fetch_cars_from_dbpedia
-from recommender.matrix import content_matrix
-
-app = Flask(__name__)
-
+from recommender.matrix import get_encoded_cars, get_weighted_matrix
+from recommender.profile import get_extended_score, get_profile, get_weighted_profile, save_scores
+from recommender.recommender import get_content_recommendation
 
 cars = fetch_cars_from_dbpedia()
-matrix_index, matrix = content_matrix(cars)
+encoded_cars_index, encoded_cars = get_encoded_cars(cars)
 
+app = Flask(__name__)
 app.cars = cars
-app.content_matrix = matrix
+app.encoded_cars = encoded_cars
+
+cars_sample: list = []
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', cars=sample(cars, 5))
+    # enumerate para preservar el index original
+    # pues este retorna una tupla (index, value)
+    cars_sample = sample(list(enumerate(cars)), 5)
+    return render_template('index.html', cars=cars_sample)
+
+
+@app.route('/car/<id>')
+def get_car(id: int):
+    return jsonify(encoded_cars.iloc[int(id)].to_dict())
 
 
 @app.route('/recommend-car', methods=['POST'])
 def recommend_car():
-    return render_template('recommendation.html')
+    if request.method != 'POST':
+        return redirect('/')
+    else:
+        data = request.form.to_dict()
+        scores = {}
+        items = data.items()
+        for index, value in items:
+            scores[int(index)] = int(value)
+        # Guardamos el perfil del usuario en el json
+        # `resources/data/profiles.json`
+        save_scores(scores)
+
+        extended_scores = get_extended_score(
+            scores, imputate_with=0, length=len(encoded_cars))
+        weighted_matrix = get_weighted_matrix(
+            encoded_matrix=encoded_cars, weight_scores=extended_scores)
+        profile = get_profile(weighted_matrix)
+        weighted_profile = get_weighted_profile(profile, extended_scores)
+        indexes = get_content_recommendation(weighted_profile, encoded_cars)
+
+        return render_template('recommendation.html', recommended=indexes.tolist(), cars=cars)
+
+
+@app.route('/test')
+def test():
+    scores = get_extended_score(scores={
+        "275": 5,
+        "82": 6,
+        "463": 8,
+        "459": 5,
+        "72": 9
+    }, imputate_with=0, length=len(encoded_cars))
+    weighted_matrix = get_weighted_matrix(
+        encoded_matrix=encoded_cars, weight_scores=scores)
+    profile = get_profile(weighted_matrix)
+    weighted_profile = get_weighted_profile(profile, scores)
+    indexes = get_content_recommendation(weighted_profile, encoded_cars)
+    return jsonify(indexes.tolist())
--- recommender/matrix.py
+++ recommender/matrix.py
@@ -1,14 +1,22 @@
 import pandas as pd
+import numpy as np
 
 columns: list[str] = ['uri', 'name', 'description', 'thumbnail', 'style',
-                      'manufacturer', 'class', 'year', 'engine', 'layout']
+                      'manufacturer', 'layout']
 
-cat_columns: list[str] = ['style', 'manufacturer', 'class', 'layout']
+cat_columns: list[str] = ['style', 'manufacturer', 'layout']
 
 
-def content_matrix(data: list) -> tuple[pd.Index, pd.DataFrame]:
+def get_encoded_cars(data: list) -> tuple[pd.Index, np.ndarray]:
     df = pd.DataFrame(data, columns=columns)
-    df.set_index(keys=['uri', 'name', 'description',
+    df.set_index(['uri', 'name', 'description',
                  'thumbnail'], inplace=True)
-    matrix = pd.get_dummies(df, columns=cat_columns)
-    return matrix.index, matrix
+
+    df = pd.get_dummies(df, columns=cat_columns)
+    return df.index, df.to_numpy()
+
+
+def get_weighted_matrix(encoded_matrix: np.ndarray, weight_scores: np.ndarray):
+    weighted_matrix = (encoded_matrix.T * weight_scores).T
+    # No hay necesidad de normalizar, pues todas las variables son indicadoras.
+    return weighted_matrix
