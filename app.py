010fe8a71acb52d335226aace39458e4e1c9ec60
sesp13 <sespinosarte99@gmail.com>
2022-02-07 23:30:07 -0500

    have partial distances

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from random import sample
 from flask import Flask, jsonify, render_template, redirect, request
 from cars.cars import fetch_cars_from_dbpedia
 from recommender.matrix import get_encoded_cars, get_weighted_matrix
-from recommender.profile import get_extended_score, get_profile, get_weighted_profile, save_profile_json, save_scores
-from recommender.recommender import get_content_recommendation
+from recommender.profile import *
+from recommender.recommender import *
+import numpy as np
 
 cars = fetch_cars_from_dbpedia()
 encoded_cars_index, encoded_cars = get_encoded_cars(cars)
@@ -33,28 +34,42 @@ def recommend_car():
     if request.method != 'POST':
         return redirect('/')
     else:
+        # Get and build my scores
         data = request.form.to_dict()
-        scores = {}
+        my_scores = {}
         items = data.items()
         for index, value in items:
-            scores[int(index)] = int(value)
-        # Guardamos el perfil del usuario en el json
-        # `resources/data/profiles.json`
-        save_scores(scores)
+            my_scores[int(index)] = int(value)
+        my_extended_scores = get_extended_score(
+            my_scores, imputate_with=0, length=len(encoded_cars))
 
-        extended_scores = get_extended_score(
-            scores, imputate_with=0, length=len(encoded_cars))
-        weighted_matrix = get_weighted_matrix(
-            encoded_matrix=encoded_cars, weight_scores=extended_scores)
-        profile = get_profile(weighted_matrix)
-        save_profile_json(profile)
-        weighted_profile = get_weighted_profile(profile, extended_scores)
-        indexes = get_content_recommendation(weighted_profile, encoded_cars)
+        # Get other people scores
+        people_scores = get_scores()
+        people_extended = np.array([get_extended_score(
+            p, imputate_with=0, length=len(encoded_cars)) for p in people_scores])
+        # Solver problems with shapes
+        people_extended_flat = people_extended.reshape(
+            -1, people_extended.shape[-1])
 
-        return render_template('recommendation.html', recommended=indexes.tolist(), cars=cars)
+        # Calc distances
+        distances = [get_distance(my_extended_scores, p)
+                     for p in people_extended_flat]
+        distances_norm = np.array([
+            0 if dist == 0 else 1 / dist for dist in distances])
+
+        # Begin the best part reccomend!
+        sum_row = np.sum((people_extended_flat.T * distances_norm).T, axis=0)
+
+        # Read other weighted profiles
+        # weightedProfiles = get_weighted_profiles_json()
+
+        # return render_template('recommendation.html', recommended=indexes.tolist(), cars=cars)
+
+        # save_scores(my_scores)
+        return jsonify(sum_row.tolist())
 
 
-@app.route('/test')
+@ app.route('/test')
 def test():
     scores = get_extended_score(scores={
         "275": 5,
